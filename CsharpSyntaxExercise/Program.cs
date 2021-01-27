6d36a80c54cf7fbea42775df1e0199f3bb5919d6
Norman Calhoun <shakauluz73@yahoo.com>
2021-01-27 15:32:01 -0500

    first commit

---
+++ CsharpSyntaxExercise/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CsharpSyntaxExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Given the following code, change the code to implement inferred typing, string interpolation, and the ternary operator
+            //int answer = 4;
+            //string response;
+
+            //if (answer < 9)
+            //{
+            //response = answer + " is less than nine";
+            //}
+            //else
+            //{
+            //response = answer + “greater than or equal to nine”;
+            //}
+
+
+
+            int answer = 4;          
+            string response = (answer < 9) ? $"{answer}Is less than nine" : $"{answer}Greater than or equal to nine";
+            Console.WriteLine(response);
+
+           
+                         
+        }
+    }
+}
